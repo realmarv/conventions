47471e335ec3c4c87fb95cddfa7c3f86920d1421
realmarv <zahratehraninasab@gmail.com>
2022-10-28 16:56:16 +0330

    WIP2

--- eofconvention.fsx
+++ eofconvention.fsx
@@ -1,32 +1,12 @@
 open System.IO
-open System
 
-// let readLines (filePath:string) = seq {
-//     use sr = new StreamReader (filePath)
-//     while not sr.EndOfStream do
-//         yield sr.ReadLine ()
-// }
-let EOFIsLinebreak(path: string) = bool {
+let EOFIsEOL(path: string) = 
     use sr = new StreamReader (path)
     let filetext = sr.ReadToEnd()
-    (filetext |> Seq.last = '\n')
-}
+    let eofIsEol = (filetext |> Seq.last = '\n')
+    (eofIsEol, path)
 
-// readLines("/home/zahra/workshop/work/nodeffect/conventions/hello.fsx")
-// |> Array.iter (printfn "%s")
-let root = "/home/zahra/workshop/work/nodeffect/conventions/"
-Directory.GetFiles("./") 
-|> Array.map Path.GetFileName 
-|> Array.iter (printfn "%s") 
-// let path = "/home/zahra/workshop/work/nodeffect/conventions/hello.fsx"
-// use lines = StreamReader.ReadToEnd(path)
-
-// printfn "HERE ===> %b" (filetext |> Seq.last = '\n')
-
-// printfn "%s" lines;;
-// let content = File.Open(path, FileMode.Open, FileAcces.Read)                
-// printfn content
-// To check
-// let lineLength = lines.Length
-// printfn "Lines length: %i" lineLength
-// lines |> Seq.iter(fun x -> printfn  "%s end" x) 
+printfn "%s" EOFIsEOL "./.gitignore"
+// Directory.GetFiles("./") 
+// |> Array.map EOFIsEOL 
+// |> Array.iter (printfn "%b*%s")
