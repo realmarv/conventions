d740d80b6c801cae35f03ee7ca82fb178366f686
realmarv <zahratehraninasab@gmail.com>
2022-11-24 15:38:53 +0330

    commitlint.config: remove code blocks first

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -362,12 +362,12 @@ module.exports = {
                         // workaround for https://github.com/conventional-changelog/commitlint/issues/3412
                         let bodyStr = rawStr.substring(lineBreakIndex);
 
+                        bodyStr = removeAllCodeBlocks(bodyStr).trim();
                         for (let paragraph of bodyStr.trim().split('\n\n')){
 
-                            paragraph = removeAllCodeBlocks(paragraph).trim();
-                            console.log('paragraph:' + paragraph)
-                            if (paragraph.length == 0)
-                                continue;
+                            // console.log('paragraph:' + paragraph)
+                            // if (paragraph.length == 0)
+                            //     continue;
 
                             let startWithLowerCase = isLowerCase(paragraph[0]);
 
