74230702001eb65a2018b0908de21e662bd45044
realmarv <zahratehraninasab@gmail.com>
2022-12-18 15:18:28 +0330

    commitlintplugins.test: remove another line

--- commitlintplugins.test.ts
+++ commitlintplugins.test.ts
@@ -100,9 +100,7 @@ failed error. Since we need to catch it, as a result of
 replacing generic try-with which lead to a red CI in the
 PR, we had to define its special excpetion and catch that:
 
-\`\`\`The active test run was aborted. Reason: Test host process crashed : Unhandled exception. System.Exception: Key handshake failed!
-
-   at System.Threading.ThreadPoolWorkQueue.Dispatch()\`\`\`
+\`\`\`The active test run was aborted. Reason: Test host process crashed : Unhandled exception. System.Exception: Key handshake failed!\`\`\`
 `
     let bodyProse10 = runCommitLintOnMsg(commitMsgWithLargeBody);
     console.log('==========>' + bodyProse10.stdout)
