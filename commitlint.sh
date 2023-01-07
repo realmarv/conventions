4852fad6c3dc8a20b31f10491af6948d5a72ed4a
realmarv <zahratehraninasab@gmail.com>
2023-01-07 15:40:14 +0330

    commitlint: install dependencies

--- commitlint.sh
+++ commitlint.sh
@@ -3,6 +3,9 @@ set -euxo pipefail
 
 # cd to directory of this script
 cd "$(dirname "$0")"
+npm install --save commitlint@17.4.0 @commitlint/cli@17.4.0 conventional-changelog-conventionalcommits
+npm i --save-dev conventional-changelog-conventionalcommits
+npm install @commitlint/config-conventional
 npx commitlint --version
 npx commitlint $@
 cd ..
