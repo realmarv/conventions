ff24574e1ee0a55e5ef3e4d30b2e3191769a54dc
realmarv <zahratehraninasab@gmail.com>
2023-01-09 15:38:04 +0330

    ci: compile commitlint.config.ts

--- commitlint.sh
+++ commitlint.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 # cd to directory of this script
 cd "$(dirname "$0")"
-npm install typescript -g
+npm install typescript -g --target es2017
 npm i --save-dev @types/node
 npm install --save commitlint@17.4.0 @commitlint/cli@17.4.0 conventional-changelog-conventionalcommits
 npm i --save-dev conventional-changelog-conventionalcommits
