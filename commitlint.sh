760422b844c6999fd6ea87f1d3ce94147fc9b4b9
realmarv <zahratehraninasab@gmail.com>
2023-01-09 15:28:48 +0330

    ci/commitlint: install typescript

--- commitlint.sh
+++ commitlint.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 
 # cd to directory of this script
 cd "$(dirname "$0")"
+npm install typescript -g
 npm i --save-dev @types/node
 npm install --save commitlint@17.4.0 @commitlint/cli@17.4.0 conventional-changelog-conventionalcommits
 npm i --save-dev conventional-changelog-conventionalcommits
