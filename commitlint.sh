11517e89ae6fb0db46fc2cfba4e3628d25b171f9
realmarv <zahratehraninasab@gmail.com>
2023-01-09 13:05:08 +0330

    commitlint: install from fork

--- commitlint.sh
+++ commitlint.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 
 # cd to directory of this script
 cd "$(dirname "$0")"
+npm install git+https://github.com/realmarv/commitlint.git
 npx commitlint --version
 npx commitlint $@
 cd ..
