93fc3e8703e8d51d993297ec8e445cfc3944e1d9
realmarv <zahratehraninasab@gmail.com>
2023-01-10 13:45:51 +0330

    ci/commitlint: install using -g

--- commitlint.sh
+++ commitlint.sh
@@ -3,10 +3,10 @@ set -euxo pipefail
 
 # cd to directory of this script
 cd "$(dirname "$0")"
-npm install npm@8.15.0
-npm install --save commitlint@17.4.0 @commitlint/cli@17.4.0 conventional-changelog-conventionalcommits
-npm i --save-dev conventional-changelog-conventionalcommits
-npm install @commitlint/config-conventional@17.1.0
+npm install -g npm@8.15.0
+npm install -g --save commitlint@17.4.0 @commitlint/cli@17.4.0 conventional-changelog-conventionalcommits
+npm i -g --save-dev conventional-changelog-conventionalcommits
+npm install -g @commitlint/config-conventional@17.1.0
 node --version
 npm --version
 npx commitlint --version
