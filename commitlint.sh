c575577a5eb107648fdbcdb0f2b1d42c6001045a
realmarv <zahratehraninasab@gmail.com>
2023-01-31 14:58:10 +0330

    commitlint: install conventionalcommits

--- commitlint.sh
+++ commitlint.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 
 # cd to directory of this script
 cd "$(dirname "$0")"
+npm install conventional-changelog-conventionalcommits
 npx commitlint --version
 npx commitlint $@
 cd ..
