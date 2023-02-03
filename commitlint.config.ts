7ff477470fd060ae5248d11b9e2677b1cd7aace3
realmarv <zahratehraninasab@gmail.com>
2023-02-03 14:40:31 +0330

    WIP8: transfer body-prose rule to Plugins class

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -52,79 +52,8 @@ module.exports = {
         {
             rules: {
                 'body-prose': ({raw}: {raw:any}) => {
-                    let offence = false;
-
                     let rawStr = Helpers.convertAnyToString(raw, "raw").trim();
-                    let lineBreakIndex = rawStr.indexOf('\n');
-
-                    if (lineBreakIndex >= 0){
-                        // Extracting bodyStr from rawStr rather than using body directly is a
-                        // workaround for https://github.com/conventional-changelog/commitlint/issues/3412
-                        let bodyStr = rawStr.substring(lineBreakIndex);
-
-                        bodyStr = Helpers.removeAllCodeBlocks(bodyStr).trim();
-                        
-                        if (bodyStr !== ''){
-
-                            function paragraphHasValidEnding(paragraph: string): boolean {
-
-                                let paragraphWords = paragraph.split(' ');
-                                let lastWordInParagraph = paragraphWords[paragraphWords.length - 1];
-                                let isParagraphEndingWithUrl = Helpers.isValidUrl(lastWordInParagraph);
-                                if (isParagraphEndingWithUrl){
-                                    return true
-                                }
-
-                                let endingChar = paragraph[paragraph.length - 1];
-                                if (endingChar === '.' ||
-                                    endingChar === ':' ||
-                                    endingChar === '!' ||
-                                    endingChar === '?') {
-                                    return true;
-                                }
-                                if (endingChar === ')' && paragraph.length > 1 &&
-                                    paragraphHasValidEnding(paragraph[paragraph.length - 2])) {
-                                    return true;
-                                }
-                                return false;
-                            }
-
-                            for (let paragraph of bodyStr.split('\n\n')){
-                                
-                                paragraph = paragraph.trim()
-
-                                if (paragraph === ''){
-                                    continue
-                                }
-
-                                let startWithLowerCase = Helpers.isLowerCase(paragraph[0]);
-
-                                let validParagraphEnd = paragraphHasValidEnding(paragraph);
-
-                                let lines = paragraph.split(/\r?\n/);
-
-                                if (startWithLowerCase) {
-                                    if (!(lines.length == 1 && Helpers.isValidUrl(lines[0]))) {
-                                        offence = true;
-                                    }
-                                }
-
-                                if (!validParagraphEnd &&
-                                    !Helpers.isValidUrl(lines[lines.length - 1]) &&
-                                    !Helpers.isFooterNote(lines[lines.length - 1])) {
-
-                                    offence = true;
-                                }
-                            }
-                                            
-                        }
-                    }
-
-                    return [
-                        !offence,
-                        `Please begin a paragraph with uppercase letter and end it with a dot.`
-                            + Helpers.errMessageSuffix
-                    ];
+                    return Plugins.bodyProse(rawStr)
                 },
 
                 'commit-hash-alone': ({raw}: {raw:any}) => {
