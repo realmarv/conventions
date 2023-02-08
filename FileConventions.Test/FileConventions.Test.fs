babe1f895755df1e69a0908663f0325e93b30eab
realmarv <zahratehraninasab@gmail.com>
2023-02-08 16:22:49 +0330

    FileConventions.Test: add fsharp project

---
+++ FileConventions.Test/FileConventions.Test.fs
@@ -0,0 +1,28 @@
+module FileConventions.Test
+
+open System
+open System.IO
+open FileConventions
+open NUnit.Framework
+open NUnit.Framework.Constraints
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let HasShebangTest1 () =
+    let fileInfo = (FileInfo $"{__SOURCE_DIRECTORY__}{Path.DirectorySeparatorChar}DummyWithoutShebang.fsx")
+    Assert.IsFalse(HasShebang(fileInfo))
+
+
+[<Test>]
+let HasShebangTest2 () =
+    let fileInfo = (FileInfo $"{__SOURCE_DIRECTORY__}{Path.DirectorySeparatorChar}DummyWithShebang.fsx")
+    Assert.IsTrue(HasShebang(fileInfo))
+
+
+[<Test>]
+let HasShebangTest3 () =
+    let fileInfo = (FileInfo $"{__SOURCE_DIRECTORY__}{Path.DirectorySeparatorChar}DummyWithWrongShebang.fsx")
+    Assert.IsFalse(HasShebang(fileInfo))
