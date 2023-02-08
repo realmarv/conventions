bd1761e9c3df02ccdd9578eb6235895e107f5210
realmarv <zahratehraninasab@gmail.com>
2023-02-08 16:24:29 +0330

    FileConventions: add fsharp project

---
+++ FileConventions/Library.fs
@@ -0,0 +1,14 @@
+﻿module FileConventions
+
+open System.Linq
+open System.IO
+open System
+
+let HasShebang (fileInfo: FileInfo) =
+    let fileText = File.ReadLines(fileInfo.FullName).First()
+
+    (
+        fileText.StartsWith("#!/usr/bin/env fsx") || 
+        fileText.StartsWith("#!/usr/bin/env -S dotnet fsi")
+    )
+
