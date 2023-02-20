ef0bfcd3282e25ac12bb56a23f247c2849974f37
realmarv <zahratehraninasab@gmail.com>
2023-02-20 11:42:58 +0330

    wip2

--- src/FileConventions/Library.fs
+++ src/FileConventions/Library.fs
@@ -19,5 +19,5 @@ let HasCorrectShebang (fileInfo: FileInfo) =
 
 let IsExecutable (fileInfo: FileInfo) =
     let hasExecuteAccess = Syscall.access(fileInfo.FullName, AccessModes.X_OK)
-    printfn "%A" hasExecuteAccess
+    printfn "==> %A" hasExecuteAccess
     hasExecuteAccess = 0
