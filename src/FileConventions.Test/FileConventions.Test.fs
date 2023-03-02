136a375ed468b07e5857f1dcbbd04c0cc67747b2
realmarv <zahratehraninasab@gmail.com>
2023-03-02 14:27:44 +0330

    *.fsx,*.fs: formatting F# files using fantomless

--- src/FileConventions.Test/FileConventions.Test.fs
+++ src/FileConventions.Test/FileConventions.Test.fs
@@ -9,72 +9,147 @@ open NUnit.Framework.Constraints
 open FileConventions
 
 [<SetUp>]
-let Setup () =
+let Setup() =
     ()
 
-let dummyFilesDirectory = DirectoryInfo (Path.Combine(__SOURCE_DIRECTORY__, "DummyFiles"))
+let dummyFilesDirectory =
+    DirectoryInfo(Path.Combine(__SOURCE_DIRECTORY__, "DummyFiles"))
 
 [<Test>]
-let HasCorrectShebangTest1 () =
-    let fileInfo = (FileInfo (Path.Combine(dummyFilesDirectory.FullName, "DummyWithoutShebang.fsx")))
+let HasCorrectShebangTest1() =
+    let fileInfo =
+        (FileInfo(
+            Path.Combine(
+                dummyFilesDirectory.FullName,
+                "DummyWithoutShebang.fsx"
+            )
+        ))
+
     Assert.That(HasCorrectShebang fileInfo, Is.EqualTo false)
 
 
 [<Test>]
-let HasCorrectShebangTest2 () =
-    let fileInfo = (FileInfo (Path.Combine(dummyFilesDirectory.FullName, "DummyWithShebang.fsx")))
+let HasCorrectShebangTest2() =
+    let fileInfo =
+        (FileInfo(
+            Path.Combine(dummyFilesDirectory.FullName, "DummyWithShebang.fsx")
+        ))
+
     Assert.That(HasCorrectShebang fileInfo, Is.EqualTo true)
 
 
 [<Test>]
-let HasCorrectShebangTest3 () =
-    let fileInfo = (FileInfo (Path.Combine(dummyFilesDirectory.FullName, "DummyWithWrongShebang.fsx")))
+let HasCorrectShebangTest3() =
+    let fileInfo =
+        (FileInfo(
+            Path.Combine(
+                dummyFilesDirectory.FullName,
+                "DummyWithWrongShebang.fsx"
+            )
+        ))
+
     Assert.That(HasCorrectShebang fileInfo, Is.EqualTo false)
 
 
 [<Test>]
 let HasCorrectShebangTest4() =
-    let fileInfo = (FileInfo (Path.Combine(dummyFilesDirectory.FullName, "DummyEmpty.fsx")))
+    let fileInfo =
+        (FileInfo(Path.Combine(dummyFilesDirectory.FullName, "DummyEmpty.fsx")))
+
     Assert.That(HasCorrectShebang fileInfo, Is.EqualTo false)
 
 
 [<Test>]
-let MixedLineEndingsTest1 () =
-    let fileInfo = (FileInfo (Path.Combine(dummyFilesDirectory.FullName, "DummyWithMixedLineEndings")))
+let MixedLineEndingsTest1() =
+    let fileInfo =
+        (FileInfo(
+            Path.Combine(
+                dummyFilesDirectory.FullName,
+                "DummyWithMixedLineEndings"
+            )
+        ))
+
     Assert.That(MixedLineEndings fileInfo, Is.EqualTo true)
 
 
 [<Test>]
-let MixedLineEndingsTest2 () =
-    let fileInfo = (FileInfo (Path.Combine(dummyFilesDirectory.FullName, "DummyWithLFLineEndings")))
+let MixedLineEndingsTest2() =
+    let fileInfo =
+        (FileInfo(
+            Path.Combine(dummyFilesDirectory.FullName, "DummyWithLFLineEndings")
+        ))
+
     Assert.That(MixedLineEndings fileInfo, Is.EqualTo false)
 
 
 [<Test>]
-let MixedLineEndingsTest3 () =
-    let fileInfo = (FileInfo (Path.Combine(__SOURCE_DIRECTORY__, "DummyFiles", "DummyWithCRLFLineEndings")))
+let MixedLineEndingsTest3() =
+    let fileInfo =
+        (FileInfo(
+            Path.Combine(
+                __SOURCE_DIRECTORY__,
+                "DummyFiles",
+                "DummyWithCRLFLineEndings"
+            )
+        ))
+
     Assert.That(MixedLineEndings fileInfo, Is.EqualTo false)
 
 
 [<Test>]
-let DetectUnpinnedVersionsInGitHubCI1 () =
-    let fileInfo = (FileInfo (Path.Combine(__SOURCE_DIRECTORY__, "DummyFiles", "DummyCIWithLatestTag.yml")))
+let DetectUnpinnedVersionsInGitHubCI1() =
+    let fileInfo =
+        (FileInfo(
+            Path.Combine(
+                __SOURCE_DIRECTORY__,
+                "DummyFiles",
+                "DummyCIWithLatestTag.yml"
+            )
+        ))
+
     Assert.That(DetectUnpinnedVersionsInGitHubCI fileInfo, Is.EqualTo true)
 
 
 [<Test>]
-let DetectUnpinnedVersionsInGitHubCI2 () =
-    let fileInfo = (FileInfo (Path.Combine(__SOURCE_DIRECTORY__, "DummyFiles", "DummyCIWithoutLatestTag.yml")))
+let DetectUnpinnedVersionsInGitHubCI2() =
+    let fileInfo =
+        (FileInfo(
+            Path.Combine(
+                __SOURCE_DIRECTORY__,
+                "DummyFiles",
+                "DummyCIWithoutLatestTag.yml"
+            )
+        ))
+
     Assert.That(DetectUnpinnedVersionsInGitHubCI fileInfo, Is.EqualTo false)
 
 
 [<Test>]
-let DetectAsteriskInPackageReferenceItems1 () =
-    let fileInfo = (FileInfo (Path.Combine(__SOURCE_DIRECTORY__, "DummyFiles", "DummyFsprojWithAsterisk.fsproj")))
+let DetectAsteriskInPackageReferenceItems1() =
+    let fileInfo =
+        (FileInfo(
+            Path.Combine(
+                __SOURCE_DIRECTORY__,
+                "DummyFiles",
+                "DummyFsprojWithAsterisk.fsproj"
+            )
+        ))
+
     Assert.That(DetectAsteriskInPackageReferenceItems fileInfo, Is.EqualTo true)
 
 
 [<Test>]
-let DetectAsteriskInPackageReferenceItems2 () =
-    let fileInfo = (FileInfo (Path.Combine(__SOURCE_DIRECTORY__, "DummyFiles", "DummyFsprojWithoutAsterisk.fsproj")))
-    Assert.That(DetectAsteriskInPackageReferenceItems fileInfo, Is.EqualTo false)
+let DetectAsteriskInPackageReferenceItems2() =
+    let fileInfo =
+        (FileInfo(
+            Path.Combine(
+                __SOURCE_DIRECTORY__,
+                "DummyFiles",
+                "DummyFsprojWithoutAsterisk.fsproj"
+            )
+        ))
+
+    Assert.That(
+        DetectAsteriskInPackageReferenceItems fileInfo,
+        Is.EqualTo false
+    )
