9feb09ad4e030fece63eb02a17c36c6ddafef23b
realmarv <zahratehraninasab@gmail.com>
2023-03-02 13:46:34 +0330

    wip1

--- scripts/compileFsharpScripts.fsx
+++ scripts/compileFsharpScripts.fsx
@@ -3,7 +3,7 @@
 open System
 open System.IO
 
-#r "nuget: Fsdk, 0.6.0--date20230214-0422.git-1ea6f62"
+#r "nuget: Fsdk, Version=0.6.0--date20230214-0422.git-1ea6f62"
 #load "../src/FileConventions/Helpers.fs"
 
 Fsdk
