1b12f79e8fd15bfcd617db4f2c2fc435c1781884
realmarv <zahratehraninasab@gmail.com>
2023-03-15 17:00:51 +0330

    scripts/inconsistentVersionsInGitHubCI.fsx: add

---
+++ scripts/inconsistentVersionsInGitHubCI.fsx
@@ -0,0 +1,15 @@
+#!/usr/bin/env -S dotnet fsi
+
+open System.IO
+
+#load "../src/FileConventions/Library.fs"
+
+let rootDir =
+    Path.Combine(__SOURCE_DIRECTORY__, "../.github/workflows") |> DirectoryInfo
+
+let inconsistentVersionsInGitHubCI =
+    FileConventions.DetectInconsistentVersionsInGitHubCI rootDir
+
+if inconsistentVersionsInGitHubCI then
+    failwith
+        "You shouldn't use inconsistent versions in `uses: foo@bar` or `with: foo-version: bar` statements in GitHubCI files."
