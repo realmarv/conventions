e5c90ea34a276b2b83e0761769fe845812e3ca8f
realmarv <zahratehraninasab@gmail.com>
2023-03-15 17:07:49 +0330

    scripts/inconsistentVersionsInFSharpScripts: add

---
+++ scripts/inconsistentVersionsInFSharpScripts.fsx
@@ -0,0 +1,14 @@
+#!/usr/bin/env -S dotnet fsi
+
+open System.IO
+
+#load "../src/FileConventions/Library.fs"
+
+let rootDir = Path.Combine(__SOURCE_DIRECTORY__, "..") |> DirectoryInfo
+
+let inconsistentVersionsInFsharpScripts =
+    FileConventions.DetectInconsistentVersionsInFsharpScripts rootDir
+
+if inconsistentVersionsInFsharpScripts then
+    failwith
+        "You shouldn't use inconsistent versions in nuget package references of F# scripts."
