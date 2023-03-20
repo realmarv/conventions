e8802a5f8579152751d1aafb146b5ea8dbe191b8
realmarv <zahratehraninasab@gmail.com>
2023-03-20 14:36:31 +0330

    FileConventions: implement the function
    Implement NonVerboseFlagsInGitHubCI function.

--- src/FileConventions/Library.fs
+++ src/FileConventions/Library.fs
@@ -107,4 +107,9 @@ let EolAtEof(fileInfo: FileInfo) =
 
 let NonVerboseFlagsInGitHubCI(fileInfo: FileInfo) =
     assert (fileInfo.FullName.EndsWith(".yml"))
-    false
+    let fileText = File.ReadAllText(fileInfo.FullName)
+
+    let nonVerboseFlagsRegex =
+        Regex("\\s-[a-zA-Z]\\s", RegexOptions.Compiled)
+
+    nonVerboseFlagsRegex.IsMatch fileText
