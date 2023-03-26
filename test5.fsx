092ef9d82fe7389a63e5b69f833bd1bf97e5ee2d
realmarv <zahratehraninasab@gmail.com>
2023-03-26 10:01:41 +0330

    wip1

---
+++ test5.fsx
@@ -0,0 +1,19 @@
+open System.IO
+// #r "../../../bin/FSharp.Data.dll"
+// #r "FSharp.Json.dll"
+// open FSharp.Json
+// open System.Json
+// open FSharp.Data.JsonProvider
+open System.Text.RegularExpressions
+#r "nuget: FSharp.Data, Version=5.0.2"
+open FSharp.Data
+// open Newtonsoft.Json.Linq
+
+// let repoRegex = Regex("\"full_name\"\\s*:\\s*\"([^\\s]*)\"", RegexOptions.Compiled)
+let jsonString = File.ReadAllText("repo_info.json")
+
+let parsedJson = JsonProvider<jsonString>
+printfn "%A" parsedJson
+// printfn "%A" ((repoRegex.Matches jsonString).[0].Groups.[1])
+
+// let info = JsonValue.Parse(jsonString)
