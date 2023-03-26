b46b9daacbfe78ccd63c3e0cb78cd53be9de0e7e
realmarv <zahratehraninasab@gmail.com>
2023-03-26 11:26:37 +0330

    wip18

--- test5.fsx
+++ test5.fsx
@@ -12,23 +12,23 @@ open FSharp.Data
 // let repoRegex = Regex("\"full_name\"\\s*:\\s*\"([^\\s]*)\"", RegexOptions.Compiled)
 let jsonString = File.ReadAllText("repo_info.json")
 
-let parsedJsonObj = FSharp.Data.JsonValue.Parse jsonString
+let parsedJsonObj = JsonProvider<jsonString>
 
-let gitRepo =
-    match parsedJsonObj.TryGetProperty "pull_request" with
-    | None -> failwith "Invalid JSON"
-    | Some pull_request ->
-        match pull_request.TryGetProperty "base" with
-        | None -> failwith "Invalid JSON"
-        | Some baseInfo ->
-            match baseInfo.TryGetProperty "repo" with
-            | None -> failwith "Invalid JSON"
-            | Some repo ->
-                match repo.TryGetProperty "full_name" with
-                | None -> failwith "Invalid JSON"
-                | Some full_name -> full_name
+// let gitRepo =
+//     match parsedJsonObj.TryGetProperty "pull_request" with
+//     | None -> failwith "Invalid JSON"
+//     | Some pull_request ->
+//         match pull_request.TryGetProperty "base" with
+//         | None -> failwith "Invalid JSON"
+//         | Some baseInfo ->
+//             match baseInfo.TryGetProperty "repo" with
+//             | None -> failwith "Invalid JSON"
+//             | Some repo ->
+//                 match repo.TryGetProperty "full_name" with
+//                 | None -> failwith "Invalid JSON"
+//                 | Some full_name -> full_name
     
-printfn "%A" gitRepo
+printfn "%A" parsedJsonObj
 // printfn "%A" ((repoRegex.Matches jsonString).[0].Groups.[1])
 
 // let info = JsonValue.Parse(jsonString)
