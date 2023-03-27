88eb788cfaf75a2b1e40d63b50ce4a47d7ad87b9
realmarv <zahratehraninasab@gmail.com>
2023-03-27 14:51:15 +0330

    wip19

--- test10.fsx
+++ test10.fsx
@@ -2,8 +2,10 @@ open System.IO
 #r "nuget: FSharp.Data, Version=5.0.2"
 open FSharp.Data
 
+[<Literal>]
+let ResolutionFolder = __SOURCE_DIRECTORY__
 // let checkSuitsType = File.ReadAllText("scripts/checkSuitsType.json")
-type Simple = JsonProvider<Path.Combine(__SOURCE_DIRECTORY__, "scripts", "checkSuitsType.json")>
+type Simple = JsonProvider<"scripts/checkSuitsType.json", ResolutionFolder=ResolutionFolder>
 
 let sample = File.ReadAllText("scripts/sample.json")
 let value = Simple.Parse sample
