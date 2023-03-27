796f957f6681d25824430577e538da8b2828afa7
realmarv <zahratehraninasab@gmail.com>
2023-03-27 14:25:15 +0330

    wip15

--- test10.fsx
+++ test10.fsx
@@ -2,9 +2,9 @@ open System.IO
 #r "nuget: FSharp.Data, Version=5.0.2"
 open FSharp.Data
 
-let checkSuitsType = File.ReadAllText("scripts/checkSuitsType.json")
+// let checkSuitsType = File.ReadAllText("scripts/checkSuitsType.json")
 
-type Simple = JsonProvider<Sample=checkSuitsType>
+type Simple = JsonProvider<"../data/checkSuitsType.json">
 
 let sample = File.ReadAllText("scripts/sample.json")
 let value = Simple.Parse sample
