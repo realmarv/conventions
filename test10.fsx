db1ca34539929d2c63b2211c9e296dfd238d95d6
realmarv <zahratehraninasab@gmail.com>
2023-03-27 13:39:08 +0330

    wip5

--- test10.fsx
+++ test10.fsx
@@ -2,8 +2,10 @@ open System.IO
 #r "nuget: FSharp.Data, Version=5.0.2"
 open FSharp.Data
 
-type Simple = JsonProvider<"scripts/checkSuitsType.json">
+let checkSuitsType = File.ReadAllText("scripts/checkSuitsType.json")
+type Simple = JsonProvider<checkSuitsType>
 
-let value = Simple.Parse(File.ReadAllText "scripts/sample.json")
+let sample = File.ReadAllText("scripts/sample.json")
+let value = Simple.Parse(sample)
 
 printfn "value: %A" value
