5978221b827143826f72834430638e087a77a237
realmarv <zahratehraninasab@gmail.com>
2023-03-28 17:02:35 +0330

    FileConventions: fix the function
    Fix DetectNotUsingSnakeCaseInScriptName function.

--- src/FileConventions/Library.fs
+++ src/FileConventions/Library.fs
@@ -125,4 +125,5 @@ let EolAtEof(fileInfo: FileInfo) =
 
 let DetectNotUsingSnakeCaseInScriptName(fileInfo: FileInfo) =
     let fileName = fileInfo.Name
-    not(fileName.ToLower() = fileName)
+    let snakeCase = fileName.ToLower() = fileName && not(fileName.Contains "-")
+    not(snakeCase)
