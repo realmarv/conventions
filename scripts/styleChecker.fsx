67cd86e9f58cdfcb8e632429e135466e07f9226c
realmarv <zahratehraninasab@gmail.com>
2023-04-04 13:39:57 +0330

    scripts/styleChecker.fsx: add
    Add styleChecker.fsx script to check style of our F#, TS
    and YML codes.
    
    The `git respore package.json` command in the
    styleChecker.fsx script was failing with the following
    error, even when I was running the
    `git config --global --add safe.directory '*'` command in
    both the styleChecker.fsx script and in the CI. In the
    [1] issue, it's suggested by someone to use --system
    instead of --global in the mentioned command, when the
    git command is running in a container, which solved the
    problem.
    
    ```
    fatal: detected dubious ownership in repository at '/__w/conventions/conventions'
    To add an exception for this directory, call:
    
    	git config --global --add safe.directory /__w/conventions/conventions
    
    Error when running 'git restore package.json'
    Fsdk.Process+ProcessFailed: Exception of type 'Fsdk.Process+ProcessFailed' was thrown.
       at Fsdk.Process.ProcessResult.Unwrap(String errMsg)
       at Fsdk.Process.ProcessResult.UnwrapDefault()
       at FSI_0002.RunPrettier(String arguments)
       at <StartupCode$FSI_0002>.$FSI_0002.main@()
    Stopped due to error
    Error: Process completed with exit code 1.
    ```
    
    [1] https://github.com/actions/checkout/issues/1048

---
+++ scripts/styleChecker.fsx
@@ -0,0 +1,145 @@
+#!/usr/bin/env -S dotnet fsi
+
+#r "nuget: Fsdk, Version=0.6.0--date20230326-0544.git-5c4f55b"
+
+open System
+
+open Fsdk
+open Fsdk.Process
+
+let StyleFSharpFiles() =
+    Process
+        .Execute(
+            {
+                Command = "dotnet"
+                Arguments = "new tool-manifest --force"
+            },
+            Process.Echo.Off
+        )
+        .UnwrapDefault()
+    |> ignore
+
+    Process
+        .Execute(
+            {
+                Command = "dotnet"
+                Arguments =
+                    "tool install fantomless-tool --version 4.7.997-prerelease"
+            },
+            Process.Echo.Off
+        )
+        .UnwrapDefault()
+    |> ignore
+
+    Process
+        .Execute(
+            {
+                Command = "dotnet"
+                Arguments = "fantomless --recurse ."
+            },
+            Process.Echo.Off
+        )
+        .UnwrapDefault()
+    |> ignore
+
+let RunPrettier(arguments: string) =
+    let processResult =
+        Process.Execute(
+            {
+                Command = "npx"
+                Arguments = $"prettier {arguments}"
+            },
+            Process.Echo.All
+        )
+
+    let errMsg =
+        sprintf
+            "Error when running '%s %s'"
+            processResult.Details.Command
+            processResult.Details.Args
+
+    match processResult.Result with
+    | Success output -> output
+    | Error(_, output) ->
+        if processResult.Details.Echo = Echo.Off then
+            output.PrintToConsole()
+            Console.WriteLine()
+            Console.Out.Flush()
+
+        Console.Error.WriteLine errMsg
+        raise <| ProcessFailed errMsg
+    | WarningsOrAmbiguous output ->
+        if processResult.Details.Echo = Echo.Off then
+            output.PrintToConsole()
+            Console.WriteLine()
+            Console.Out.Flush()
+
+        let fullErrMsg = sprintf "%s (with warnings?)" errMsg
+        fullErrMsg
+    |> printfn "%A"
+
+
+    // Since after installing commitlint dependencies package.json file changes, we need to
+    // run the following command to ignore package.json file
+    Process
+        .Execute(
+            {
+                Command = "git"
+                Arguments = "restore package.json"
+            },
+            Process.Echo.Off
+        )
+        .UnwrapDefault()
+    |> ignore
+
+let StyleTypeScriptFiles() =
+    RunPrettier "--quote-props=consistent --write ./**/*.ts"
+
+let StyleYmlFiles() =
+    RunPrettier "--quote-props=consistent --write ./**/*.yml"
+
+StyleFSharpFiles()
+StyleTypeScriptFiles()
+StyleYmlFiles()
+
+let processResult =
+    Process.Execute(
+        {
+            Command = "git"
+            Arguments = "diff --exit-code"
+        },
+        Process.Echo.Off
+    )
+
+let errMsg =
+    sprintf
+        "Error when running '%s %s'"
+        processResult.Details.Command
+        processResult.Details.Args
+
+let suggestion =
+    "Please use the following commands to style your code:"
+    + "Style your F# code using: `dotnet fantomless --recurse .`"
+    + "Style your TypeScript code using: `npx prettier --quote-props=consistent --write ./**/*.ts`"
+    + "Style your YML code using: `npx prettier --quote-props=consistent --write ./**/*.yml`"
+
+match processResult.Result with
+| Success output -> output
+| Error(_, output) ->
+    if processResult.Details.Echo = Echo.Off then
+        output.PrintToConsole()
+        Console.WriteLine()
+        Console.Out.Flush()
+
+    let fullErrMsg = suggestion + System.Environment.NewLine + errMsg
+    Console.Error.WriteLine fullErrMsg
+    raise <| ProcessFailed fullErrMsg
+| WarningsOrAmbiguous output ->
+    if processResult.Details.Echo = Echo.Off then
+        output.PrintToConsole()
+        Console.WriteLine()
+        Console.Out.Flush()
+
+    let fullErrMsg = sprintf "%s (with warnings?)" errMsg
+    fullErrMsg
+|> printfn "%A"
